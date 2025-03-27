c126410732a4dc01265e3a5b31ea201ab65fd5f1
sdrabdalfattah <sdrabdalfattah@gmail.com>
2025-03-27 03:38:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const { Server } = require("socket.io");
 require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
+
 
 
 
